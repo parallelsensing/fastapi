a41ee0620d5fe597a8a50a114e8b9ade24fb9438
Jia Zhang <sc19jz2@leeds.ac.uk>
2024-03-27 05:01:28 +0800

    添加fastapi-ally

---
+++ app/readme.md
@@ -0,0 +1,3 @@
+### 运行项目 ###
+1.Run 'main'
+2. 命令行 uvicorn main:app --reload
--- main.py
+++
@@ -1,6 +0,0 @@
-import uvicorn
-
-if __name__ == "__main__":
-  config = uvicorn.Config("app.main:app", host="0.0.0.0", port=9711)
-  server = uvicorn.Server(config)
-  server.run()   
