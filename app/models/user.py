26ec2afd5f5848d2397441322cc53c83ad123c3d
Jia Zhang <sc19jz2@leeds.ac.uk>
2024-04-01 15:57:26 +0800

    change user.py

--- app/models/user.py
+++ app/models/user.py
@@ -7,16 +7,29 @@ if TYPE_CHECKING:
   from .author import Author  # noqa: F401
 
 
-class User(Base, Common):
-  """
-  User model
-  """
-  email = Column(String, unique=True, index=True, nullable=False)
-  password = Column(String, nullable=False)
-  isActive = Column("is_active", Boolean, default=True)
-  isAdmin = Column("is_admin", Boolean, default=False)
-  authorID = Column("author_id", Integer, ForeignKey("author.id", onupdate="CASCADE", ondelete="SET NULL"))
-  roleID = Column("role_id", Integer, ForeignKey("role.id", onupdate="CASCADE", ondelete="SET NULL"))
+# class User(Base, Common):
+#   """
+#   User model
+#   """
+#   email = Column(String, unique=True, index=True, nullable=False)
+#   password = Column(String, nullable=False)
+#   isActive = Column("is_active", Boolean, default=True)
+#   isAdmin = Column("is_admin", Boolean, default=False)
+#   authorID = Column("author_id", Integer, ForeignKey("author.id", onupdate="CASCADE", ondelete="SET NULL"))
+#   roleID = Column("role_id", Integer, ForeignKey("role.id", onupdate="CASCADE", ondelete="SET NULL"))
 
-  author:Mapped["Author"] = relationship("Author")
+#   author:Mapped["Author"] = relationship("Author")
+
+  class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    password = Column(String)
+    phone = Column(String)
+
+    def __repr__(self):
+        return f"<User(username='{self.username}', first_name='{self.first_name}', last_name='{self.last_name}', phone='{self.phone}')>"
 
