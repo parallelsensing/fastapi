1049061bd9cadcc3fc70edd06a3451ee60f6d506
Jia Zhang <sc19jz2@leeds.ac.uk>
2024-04-08 01:27:51 +0800

    fix bug, it can run

--- app/main.py
+++ app/main.py
@@ -13,5 +13,5 @@ router.include_router(sensing.item, prefix="/item", tags=["sensing", "item"])
 router.include_router(sensing.user, prefix="/user", tags=["sensing", "user"])
 
 
-
+app.include_router(router, prefix="/api/sensing", tags=["sensing"])
 
