963022164c14d19872d05656eb117c28b43ebba4
Jia Zhang <sc19jz2@leeds.ac.uk>
2024-04-16 16:26:36 +0800

    add role to user db

--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,7 @@ class User(Base):
     last_name = Column(String)
     password = Column(String)
     phone = Column(String)
+    role = Column(Integer, default=1) # 0:admin, 1:normal_user
 
     def __repr__(self):
         return f"<User(username='{self.username}', first_name='{self.first_name}', last_name='{self.last_name}', phone='{self.phone}')>"
