bfb1544cd0695b0b7908cc8415c829b61a91d2c9
markyangkp <markyangkp@outlook.com>
2024-09-20 17:09:07 +0800

    验证码新增有效期

--- app/core/captcha/captcha.py
+++ app/core/captcha/captcha.py
@@ -4,13 +4,6 @@ from app.models import CaptchaModel,User
 import random
 import time
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 class Captcha:
     def __init__(self,):
         self.db = SessionLocal()
@@ -26,10 +19,15 @@ class Captcha:
         # 检查是否已经存在验证码
         captchaItem = self.get_captcha(email)
         if captchaItem:
-            return captchaItem
+            if int(time.time())-captchaItem.timestamp<=300:
+                return None
+            else:
+                self.db.delete(captchaItem)
+                self.db.commit()
+            # return captchaItem
         # 生成验证码
         code = ''.join([str(random.randint(0, 9)) for _ in range(6)])
-        timestamp = int(time.time())
+        # timestamp = int(time.time())
         captcha = CaptchaModel(code=code, email=email)
         self.db.add(captcha)
         self.db.commit()
@@ -41,10 +39,13 @@ class Captcha:
         self.db.commit()
         self.db.close()
         
-    def check_captcha(self, email:str, code:str):
-        caseptcha = self.get_captcha(email)
-        if caseptcha and caseptcha.code == code:
-            self.clear_captcha(caseptcha)
-            return True
+    def check_captcha(self, email: str, code: str) -> bool:
+        captcha = self.get_captcha(email)
+        if captcha and captcha.code == code:
+            # 检查有效期
+            current_time = int(time.time())
+            if current_time - captcha.timestamp <= 300:  # 5分钟有效期
+                self.clear_captcha(captcha)
+                return True
         return False
     
